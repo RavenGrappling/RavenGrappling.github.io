9102846e43188aa27214ae7c83260ce5142eb92a
Oleg <oleg@khabarov.ca>
2024-09-02 18:49:40 -0400

    form is functional

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,13 +1,17 @@
-window.onload = initNav;
-
-function initNav() {
-  const navCollapseButton = document.querySelector("[data-nav-toggle]");
-  navCollapseButton.addEventListener("click", toggleNav);
+window.onload = () => {
+  document
+    .querySelector("[data-nav-toggle]")
+    .addEventListener("click", toggleNav);
 
   document.querySelectorAll("[data-nav-dropdown]").forEach((element) => {
     element.addEventListener("click", toggleDropdown);
   });
-}
+
+  const leadsForm = document.querySelector("[data-leads-form]");
+  if (leadsForm) {
+    leadsForm.addEventListener("submit", processLeadsForm);
+  }
+};
 
 function toggleNav(event) {
   event.preventDefault();
@@ -17,4 +21,23 @@ function toggleNav(event) {
 function toggleDropdown(event) {
   event.preventDefault();
   event.currentTarget.nextElementSibling.classList.toggle("show");
+}
+
+function processLeadsForm(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const data = new FormData(form);
+
+  fetch(form.action, { method: "post", body: data })
+    .then((response) => {
+      if (response.ok) {
+        form.classList.add("success");
+      } else {
+        form.classList.add("failure");
+      }
+    })
+    .catch((error) => {
+      form.classList.add("failure");
+    });
 }
